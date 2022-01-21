92d5161ddbfd5a1f2b67ea10091284258f32d62e
Yuliia Skopenko <julia.skopenko@gmail.com>
2022-01-21 19:57:38 +0200

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import {Button, Message} from './components';
 
-const text = 'Homework 1'
+
 
 function App() {
+
+  const text = 'Homework 1'
+  
+  const [messageList, setMessageList] = useState([]);
+  const [value, setValue] = useState('');
+
+  const sendMessage = (author, text) => {
+    const newMessageList = [...messageList];
+    const newMessage = {author, text};
+    newMessageList.push(newMessage);
+    setMessageList(newMessageList);
+  };
+
+  const resetForm = () => {
+    setValue('');
+  };
+
+  const onSumbitMessage = (event) => {
+    event.preventDefault();
+    sendMessage('user', value);
+    resetForm();
+  };
+
+  const onChangeMessageInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    if (messageList.length === 0) {
+      return;
+    }
+
+    const tail = messageList[messageList.length - 1];
+    if (tail.author === 'bot') {
+      return;
+    }
+
+    sendMessage('bot', 'hi')
+  }, [messageList]);
+
   return (
     <div className="App">
       <Message text={text}/>
+
+       <form onSubmit={onSumbitMessage}>
+         <input
+          className="text-send"
+          onChange={onChangeMessageInput}
+          placeholder="message"
+          value={value}
+          type="text"
+         />
+          <button
+            className="button-send">
+            Send
+          </button>
+       </form>
+       <ul>
+         {
+           messageList.map((item, index) => (
+              <li key={index}>
+                {item.author} - {item.text}
+              </li>
+           ))
+         }
+       </ul>
+
 
       <Button
         color="alert">
